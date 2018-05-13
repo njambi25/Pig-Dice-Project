fae778b956287f856f444e9a0d544a2ab2273d4d
first-name second-namegit config --global user.name Alice <njambialice25@gmail.com>
2018-05-14 00:08:21 +0300

    start game button functional

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,11 @@ var game = {
   //set player names
   setPlayer1Name: function(name) {
     this.player1Name = name;
-  };
+  },
+
   setPlayer2Name: function(name) {
     this.player2Name = name;
-  };
+  },
 
 }
 $(document).ready(function() {
@@ -18,13 +19,14 @@ $(document).ready(function() {
     $("#player-names-container").hide();
     var name1 = $("input#player1Name").val();
     var name2 = $("input#player2Name").val();
-
     //user game object
     game.setPlayer1Name(name1);
     game.setPlayer2Name(name2);
 
     $("span#gameStartPlayer1Name").html(name1);
     $("span#gameStartPlayer2Name").html(name2);
+
+    $("game-container").show();
 
 
   });
