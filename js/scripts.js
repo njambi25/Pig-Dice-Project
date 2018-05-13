dc8539ba1c30c95672c1df40c54fd63b2c6857fa
first-name second-namegit config --global user.name Alice <njambialice25@gmail.com>
2018-05-13 20:15:45 +0300

    restart

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-function toggle() {
-  var x = document.getElementById("rules");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
