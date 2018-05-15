3bf9759087b0a7e624368e89e8981379b67c944c
first-name second-namegit config --global user.name Alice <njambialice25@gmail.com>
2018-05-15 19:03:50 +0300

    start game button working after click and hides the input form

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
   $("#button").click(function() {
     $(".rule").toggle()
-  })
+  });
+
+  $("#start-game-button").click(function(event) {
+    event.preventDefault();
+
+    player1 = $("#player1Name").val();
+    player2 = $("#player2Name").val();
+    $("#playerinfo").hide();
+    // display input
+    $("#gameStartPlayer1Name").text(player1);
+    $("#gameStartPlayer2Name").text(player2);
+
+  });
+
+
+
 });
